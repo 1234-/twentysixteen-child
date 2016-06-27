8c5897f4b09710fc1b557c92405bb503215ee23a
Dan West <dan@godfather.com>
2016-06-27 13:13:16 +0200

    Front end visning för artiklar

--- template-parts/content-single-artikel.php
+++ template-parts/content-single-artikel.php
@@ -17,6 +17,33 @@
 	<?php twentysixteen_excerpt(); ?>
 	
 	<div class="entry-content">
+		<div class="laddaner">
+	<?php
+	if(get_field('pdf')) {
+	echo '<h3>Ladda ner artikel:</h3>';
+	}
+	elseif(get_field('epub')) {
+	echo '<h3>Ladda ner artikel:</h3>'; }
+	elseif(get_field('mobi')) {
+	echo '<h3>Ladda ner artikel:</h3>'; }
+	else {
+	echo '<h3>Status: Digitalisering pågår</h3><p>Den här artikeln håller på att digitaliseras. Snart kommer du kunna ladda ner den i e-boksformat. Välkommen åter!</p>';
+	}
+
+if(get_field('pdf'))
+{
+	echo '<a href="' . get_field('pdf') . '" alt="Ladda ner PDF" class="btn btn-primary btn-block">.pdf</a>';
+}
+if(get_field('epub'))
+{
+	echo '<a href="' . get_field('epub') . '" alt="Ladda ner EPUB" class="btn btn-primary btn-block">.epub</a>';
+}
+if(get_field('mobi'))
+{
+	echo '<a href="' . get_field('mobi') . '" alt="Ladda ner MOBI" class="btn btn-primary btn-block">.mobi</a>';
+}
+?>
+</div>
 <?php
 			the_content();
 
@@ -35,17 +62,25 @@
 		<?php twentysixteen_entry_meta(); ?>
 	<span class="byline">Författare: <a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author"><?php echo get_the_author(); ?></a></span>
 		<?php
-if(get_field('utgiven'))
+if(get_field('publicerad'))
 {
-	echo '<span class="bokmeta">Utgiven: ' . get_field('utgiven') . '</span>';
+	echo '<span class="bokmeta">Publicerad: ' . get_field('publicerad') . '</span>';
 }
-if (has_tag())
+if(get_field('publicerad'))
 {
-	echo '<span class="bokmeta">Diktsamling: ';
-	echo  the_tags('');
-	echo '</span>';
-} ?> 
+	echo '<span class="bokmeta">Källa: ' . get_field('kalla') . '</span>';
+}
+if(get_field('digitaliserad_av'))
+{
+	echo '<span class="bokmeta">Digitaliserad av: <a href="';
+	echo the_field('digitaliserad_av_url');
+	echo '">' . get_field('digitaliserad_av') . '</a></span>';
+}
+?>
 <hr/>
+<?php
+if(get_field('licens') == "publicdomain")
+{ ?>
 <p xmlns:dct="http://purl.org/dc/terms/">
 <a rel="license" href="http://creativecommons.org/publicdomain/mark/1.0/">
 <img src="http://i.creativecommons.org/p/mark/1.0/88x31.png"
@@ -54,6 +89,16 @@ if (has_tag())
 <br />
 <small>Detta verk (<span property="dct:title"><?php the_title( ); ?></span>, av <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="dct:creator"><span property="dct:title"><?php the_author( ); ?></span></a>), gjort tillgängligt av <a href="http://projektmoberg.se/" rel="dct:publisher"><span property="dct:title">Projekt Moberg</span></a>, är allmän egendom och får spridas och användas fritt.</small>
 </p>
+<?php } elseif(get_field('licens') == "cc-by-nc-sa")
+{?>
+<p xmlns:dct="http://purl.org/dc/terms/">
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/se/">
+                    <img src="http://projektmoberg.se/wp-content/uploads/2016/03/cc-by-nc-sa-80x15.png" style="border-style: none;" alt="CC-BY-NC-SA">
+</a>
+<br />
+<small>Detta verk (<span property="dct:title"><?php the_title( ); ?></span>, av <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="dct:creator"><span property="dct:title"><?php the_author( ); ?></span></a>), gjort tillgängligt av <a href="<?php the_field('digitaliserad_av_url') ?>" rel="dct:publisher"><span property="dct:title"><?php the_field('digitaliserad_av') ?></span></a>, är licensierat med <a href="https://creativecommons.org/licenses/by-nc-sa/2.5/se/">CC BY-NC-SA</a> licens och får delas fritt, med några villkor.</small>
+</p>
+<?php } ?>
 
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
